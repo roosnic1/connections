734fb2b53ee496538e5c03d72989e40978010de3
Nico Roos <nico.roos@swisstxt.ch>
2025-06-06 16:26:44 +0200

    🌐 (examples.ts): update categories and items to reflect local Zurich themes

--- app/_examples.ts
+++ app/_examples.ts
@@ -2,23 +2,24 @@ import { Category } from "./_types";
 
 export const categories: Category[] = [
   {
-    category: 'ENCOURAGE, WITH "ON"',
-    items: ["EGG", "GOAD", "SPUR", "URGE"],
+    category: "Stadt Zürcher Quartiere",
+    items: ["Enge", "Sihlfeld", "Werd", "Unterstrass"],
     level: 1,
   },
   {
-    category: "SPHERICAL FOODS",
-    items: ["JAWBREAKER", "MEATBALL", "MOZZARELLA", "ORANGE"],
+    category: "____ Platz",
+    items: ["Limmat", "Hard", "Helvetia", "Escher-Wyss"],
     level: 2,
   },
   {
-    category: "GROCERY STORE AISLES",
-    items: ["DAIRY", "FROZEN", "PRODUCE", "SNACK"],
+    category: "Zürcher Bars",
+    items: ["Campo", "Sport", "Total", "Kasheme"],
     level: 3,
   },
+
   {
-    category: "GO ___",
-    items: ["BANANAS", "FIGURE", "FISH", "STEADY"],
+    category: "Zürcher Exekutiv Politiker:innen ohne 'er' am Ende",
+    items: ["Stock", "Stein", "Brand", "Baum"],
     level: 4,
-  },
+  }
 ];
