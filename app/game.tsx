4c5136e9fd0db17d8d74928cd137af54d66eac5d
Nico Roos <nico.roos@swisstxt.ch>
2025-06-08 20:38:24 +0200

    ✨ (game.tsx): create new game component to encapsulate game logic ♻️ (page.tsx): refactor Home component to use new Game component ✨ (prisma.ts): add Prisma client setup for database interaction ✨ (package.json): add Prisma and dotenv-cli dependencies and scripts ✨ (schema.prisma): define schema for Connection model in Prisma ✨ (seed.ts): add seed script to populate database with initial data

---
+++ app/game.tsx
@@ -0,0 +1,178 @@
+"use client";
+
+import {useCallback, useState} from "react";
+import ControlButton from "./_components/button/control-button";
+import Grid from "./_components/game/grid";
+import GameLostModal from "./_components/modal/game-lost-modal";
+import GameWonModal from "./_components/modal/game-won-modal";
+import Popup from "./_components/popup";
+import useAnimation from "./_hooks/use-animation";
+import useGameLogic from "./_hooks/use-game-logic";
+import usePopup from "./_hooks/use-popup";
+import { SubmitResult, Word } from "./_types";
+import { getPerfection } from "./_utils";
+import {useTranslations} from "next-intl";
+import {useFlag} from "@unleash/nextjs/client";
+
+
+export default function Home() {
+
+
+
+    const [popupState, showPopup] = usePopup();
+    const {
+        gameWords,
+        selectedWords,
+        clearedCategories,
+        mistakesRemaining,
+        isWon,
+        isLost,
+        guessHistoryRef,
+        selectWord,
+        shuffleWords,
+        deselectAllWords,
+        getSubmitResult,
+        handleWin,
+        handleLoss,
+    } = useGameLogic();
+
+    const [showGameWonModal, setShowGameWonModal] = useState(false);
+    const [showGameLostModal, setShowGameLostModal] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
+
+    const {
+        guessAnimationState,
+        wrongGuessAnimationState,
+        animateGuess,
+        animateWrongGuess,
+    } = useAnimation();
+
+    const t = useTranslations('HomePage');
+
+    const handleSubmit = async () => {
+        setSubmitted(true);
+        await animateGuess(selectedWords);
+
+        const result: SubmitResult = getSubmitResult();
+
+        switch (result.result) {
+            case "same":
+                showPopup(t('alreadyGuessed'));
+                break;
+            case "one-away":
+                animateWrongGuess();
+                showPopup(t('oneAway'));
+                break;
+            case "loss":
+                showPopup(t('betterLuck'));
+                await handleLoss();
+                setShowGameLostModal(true);
+                break;
+            case "win":
+                showPopup(getPerfection(mistakesRemaining));
+                await handleWin();
+                setShowGameWonModal(true);
+                break;
+            case "incorrect":
+                animateWrongGuess();
+                break;
+        }
+        setSubmitted(false);
+    };
+
+    const onClickCell = useCallback(
+        (word: Word) => {
+            selectWord(word);
+        },
+        [selectWord]
+    );
+
+    const renderControlButtons = () => {
+        const showResultsWonButton = (
+            <ControlButton
+                text={t('showResults')}
+                onClick={() => {
+                    setShowGameWonModal(true);
+                }}
+            />
+        );
+
+        const showResultsLostButton = (
+            <ControlButton
+                text={t('showResults')}
+                onClick={() => {
+                    setShowGameLostModal(true);
+                }}
+            />
+        );
+
+        const inProgressButtons = (
+            <div className="flex gap-2 mb-12">
+                <ControlButton
+                    text={t('shuffle')}
+                    onClick={shuffleWords}
+                    unclickable={submitted}
+                />
+                <ControlButton
+                    text={t('clear')}
+                    onClick={deselectAllWords}
+                    unclickable={selectedWords.length === 0 || submitted}
+                />
+                <ControlButton
+                    text={t('submit')}
+                    unclickable={selectedWords.length !== 4 || submitted}
+                    onClick={handleSubmit}
+                />
+            </div>
+        );
+
+        if (isWon) {
+            return showResultsWonButton;
+        } else if (isLost) {
+            return showResultsLostButton;
+        } else {
+            return inProgressButtons;
+        }
+    };
+
+    const isEnabled = useFlag("ask_feedback");
+
+
+
+    return (
+        <>
+            <div>
+                <h1 className="text-black text-4xl font-semibold my-4 ml-4">{t('title')}</h1>
+                <hr className="mb-4 md:mb-4 w-full"></hr>
+                <h1 className="text-black mb-4">{t('subtitle')}</h1>
+                <h2 className="text-black my-4 md:my-8 mx-8">is enabled: {isEnabled ? "ENABLED" : "DISABLED"}</h2>
+                <div className="relative w-full">
+                    <Popup show={popupState.show} message={popupState.message} />
+                    <Grid
+                        words={gameWords}
+                        selectedWords={selectedWords}
+                        onClick={onClickCell}
+                        clearedCategories={clearedCategories}
+                        guessAnimationState={guessAnimationState}
+                        wrongGuessAnimationState={wrongGuessAnimationState}
+                    />
+                </div>
+                <h2 className="text-black my-4 md:my-8 mx-8">
+                    {t('mistakesRemaining', {count: mistakesRemaining > 0 ? Array(mistakesRemaining).fill("•").toString() : ""})}
+                </h2>
+                {renderControlButtons()}
+            </div>
+            <GameWonModal
+                isOpen={showGameWonModal}
+                onClose={() => setShowGameWonModal(false)}
+                guessHistory={guessHistoryRef.current}
+                perfection={getPerfection(mistakesRemaining)}
+            />
+            <GameLostModal
+                isOpen={showGameLostModal}
+                onClose={() => setShowGameLostModal(false)}
+                guessHistory={guessHistoryRef.current}
+            />
+        </>
+    );
+}
