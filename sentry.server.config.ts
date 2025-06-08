94d2e3100d9fe1319fedee0f10fedaccbb5dc136
Nico Roos <nico.roos@swisstxt.ch>
2025-06-08 22:33:18 +0200

    ✨ (sentry): integrate Sentry for error monitoring and performance tracking
    🔧 (config): update .gitignore to exclude Sentry config file
    
    ⬆️ (dependencies): add @sentry/nextjs to package.json for Sentry integration

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d8a90d627d5b4752e46cadca7070ec52@o4509464836898816.ingest.de.sentry.io/4509464838013008",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
