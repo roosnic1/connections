a023b52571ce1b27b760bfffdcf8dc4cc4a17f11
Nico Roos <nico.roos@swisstxt.ch>
2025-06-09 15:26:34 +0200

    🐛 (page.tsx): add optional chaining to prevent runtime errors when accessing categories ♻️ (page.tsx): provide default empty array to Game component to handle cases with no categories

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,10 @@ export default async function Home() {
       categories: true,
     },
   });
+
   const categories: Category[] = connections[
     Math.floor(Math.random() * connections.length)
-  ].categories.map((category) => ({
+  ]?.categories.map((category) => ({
     category: category.title,
     items: category.words,
     level: category.level as 1 | 2 | 3 | 4,
@@ -31,7 +32,7 @@ export default async function Home() {
     <>
       <div className="flex flex-col items-center w-11/12 md:w-3/4 lg:w-7/12 mx-auto mt-14">
         <h1 className="text-black text-4xl font-semibold my-4 ml-4">Hello</h1>
-        <Game categories={categories} />
+        <Game categories={categories || []} />
       </div>
     </>
   );
