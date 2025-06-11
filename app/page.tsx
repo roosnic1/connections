1859ab3a877d00b5a4523b48a5024f019d24dd19
Nico Roos <nico.roos@swisstxt.ch>
2025-06-11 16:05:00 +0200

    ✨ (page.tsx): add timezone support using Luxon for date handling ♻️ (page.tsx): refactor to use findFirst instead of findMany for connections 📝 (seed.ts): add new seed data for categories with publish dates

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,47 @@ import prisma from "@/lib/prisma";
 import { UnleashClient } from "unleash-proxy-client";
 import { Category } from "./_types";
 import Game from "./game";
+import { DateTime } from "luxon";
 
-export default async function Home() {
-  const connections = await prisma.connection.findMany({
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const now = DateTime.now().setZone("Europe/Berlin");
+  const connection = await prisma.connection.findFirst({
+    where: {
+      publishDate: {
+        lte: now.plus({ days: 0 }).toJSDate(),
+      },
+    },
+    orderBy: [{ createdAt: "desc" }],
     //relationLoadStrategy: 'join', // or 'query'
     include: {
       categories: true,
     },
   });
 
-  const categories: Category[] = connections[
-    Math.floor(Math.random() * connections.length)
-  ]?.categories.map((category) => ({
+  /*const connections = await prisma.connection.findMany({
+    where: {
+      publishDate: {
+        lte: DateTime.now().setZone("Europe/Berlin").toJSDate(),
+      },
+    },
+    orderBy: [{createdAt: 'desc'}],
+    //relationLoadStrategy: 'join', // or 'query'
+    include: {
+      categories: true,
+    },
+  });*/
+
+  // TODO: what to do when no connection is found.
+  // @ts-ignore
+  const categories: Category[] = connection.categories.map((category) => ({
     category: category.title,
     items: category.words,
     level: category.level as 1 | 2 | 3 | 4,
   }));
-  //console.log(categories)
 
   const unleash = new UnleashClient({
     url: process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_URL || "",
@@ -30,6 +54,8 @@ export default async function Home() {
 
   const isEnabled = unleash.isEnabled("ask_feedback");
   console.log(`ASK_FEEDBACK: ${isEnabled ? "enabled" : "disabled"}`);
+  const paramsio = await searchParams;
+  console.log("params", params);
 
   return (
     <div className="flex flex-col items-center w-11/12 md:w-3/4 lg:w-7/12 mx-auto mt-14">
