7f1a16f633c3cc56c24019eea8f1f92117f91284
Nico Roos <nico.roos@swisstxt.ch>
2025-07-19 22:36:06 +0200

    🌐 (request.ts): add locale validation to ensure requested locale is supported, defaulting to DEFAULT_LANGUAGE if not

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,11 +1,17 @@
 import { getRequestConfig } from "next-intl/server";
+import { hasLocale } from "use-intl";
+import { ALL_LANGUAGES, DEFAULT_LANGUAGE } from "@/tolgee/shared";
 
 // @ts-ignore
 export default getRequestConfig(async ({ requestLocale }) => {
-  const locale = await requestLocale;
+  const requested = await requestLocale;
+  const locale = hasLocale(ALL_LANGUAGES, requested)
+    ? requested
+    : DEFAULT_LANGUAGE;
+
   return {
     // do this to make next-intl not emmit any warnings
     locale,
-    messages: { locale: locale! },
+    //messages: { locale: locale! },
   };
 });
