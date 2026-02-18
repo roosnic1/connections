ed6de494534017abe6f6db7128a846ea18f444b8
Nico Roos <nicolas.k.roos@protonmail.com>
2026-02-18 13:52:38 +0100

    🔥 Remove unused navigation.ts file
    Remove next-intl navigation utilities as they are no longer needed
    in the current implementation.

--- navigation.ts
+++
@@ -1,9 +0,0 @@
-import { createNavigation } from "next-intl/navigation";
-import { ALL_LANGUAGES } from "@/i18n/config";
-
-// read more about next-intl library
-// https://next-intl-docs.vercel.app
-export const { Link, redirect, usePathname, useRouter } = createNavigation({
-  locales: ALL_LANGUAGES,
-  localePrefix: "never",
-});
