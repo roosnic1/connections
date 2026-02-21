38625c86af0ebbade3594c016b3bdd50e1b103ea
Nico Roos <nicolas.k.roos@protonmail.com>
2026-02-21 17:18:43 +0100

    ✅ add webkit browser support for E2E tests to improve cross-browser coverage

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,10 @@ export default defineConfig({
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
   webServer: {
     command: process.env.CI
